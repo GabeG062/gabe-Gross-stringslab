72f25477e6da61175be50959d2e73b8c4bc688ef
starman062 <gabrielgross222@gmail.com>
2026-04-09 19:05:22 -0700

    changed ShiftCyber to use modern .net

--- stringslab_master/StringsLab_Core5/ShiftCypher/Program.cs
+++ stringslab_master/StringsLab_Core5/ShiftCypher/Program.cs
@@ -6,8 +6,24 @@ namespace ShiftCypher
     {
         static void Main(string[] args)
         {
-            //Changed the output to Hello Class
-            Console.WriteLine("Hello Class!");
+            Console.WriteLine("Please enter a word or phrase.  Press the ENTER key when you're done.");
+            string input = Console.ReadLine();
+            
+            string[] words = input.Split();
+            foreach (string word in words)
+                Console.WriteLine(word);
+            
+            Random rnd = new Random();
+            int shift = rnd.Next(-10, 10);
+            Console.WriteLine("Shift is " + shift);
+            
+            string encodedWord = Coder(words[0]);
+            Console.WriteLine($"the encoded version of {words[0]} with a shift of {shift} is {encodedWord}");
+        }
+
+        static string Coder(string s)
+        {
+            return "asdf";
         }
     }
 }
